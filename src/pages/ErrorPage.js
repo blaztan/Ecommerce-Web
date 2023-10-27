4619fe3da43f59c4e1e5c2787101ea8c140aae61
nandyluis323 <periksoon191@gmail.com>
2023-10-27 17:25:55 +0530

    Set up the project and Add react router

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Footer, Navbar, Sidebar } from "../components";
+const ErrorPage = () => {
+  return;
+  <>
+    <Navbar />
+    <Sidebar />
+    <h4>error page</h4>;
+    <Footer />
+  </>;
+};
+
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`;
+
+export default ErrorPage;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import AboutPage from "./AboutPage";
+import PrivateRoute from "./PrivateRoute";
+import CartPage from "./CartPage";
+import CheckoutPage from "./CheckoutPage";
+import ErrorPage from "./ErrorPage";
+import HomePage from "./HomePage";
+import ProductsPage from "./ProductsPage";
+import SingleProductPage from "./SingleProductPage";
+import RootLayout from "./root";
+
+export {
+  RootLayout,
+  AboutPage,
+  PrivateRoute,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+};
