320fe5c64379a21b4060021c948ebdd5a599e957
nandyluis323 <periksoon191@gmail.com>
2023-10-28 00:01:41 +0530

    Add Navbar Sidebar Footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,30 @@ import { useProductsContext } from "../context/products_context";
 import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
-  return <h4>navbar</h4>;
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="compfy sloth" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            return (
+              <li key={link.id}>
+                <Link to={link.url}>{link.text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  );
 };
 
 const NavContainer = styled.nav`
