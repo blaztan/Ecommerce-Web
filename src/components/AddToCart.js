2adaaa8a61f840c183d7bcbf9a8e05da950f081e
nandyluis323 <periksoon191@gmail.com>
2023-11-07 16:43:29 +0530

    Complete AddToCart cart items and total

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
@@ -58,7 +59,11 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to="/cart" className="btn">
+        <Link
+          onClick={() => addToCart(id, mainColor, amount, product)}
+          to="/cart"
+          className="btn"
+        >
           add to cart
         </Link>
       </div>
