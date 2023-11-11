7ed468b5b8ac304babc4a220fdb7dd43f6affc98
nandyluis323 <periksoon191@gmail.com>
2023-11-12 00:45:29 +0530

    edit package

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Footer, Navbar, Sidebar } from "../components";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  // Use useEffect to automatically redirect after 100ms
+
+  const redirectTimeout = setTimeout(() => {
+    navigate("/");
+  }, 50);
+
   return (
     <>
       <Navbar />
@@ -11,6 +20,7 @@ const ErrorPage = () => {
         <section>
           <h1>404</h1>
           <h3>Sorry, the page you tried cannot be found</h3>
+          {/* You can still use a Link or a button for manual redirection */}
           <Link className="btn" to="/">
             Back Home
           </Link>
